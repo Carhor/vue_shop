e1491cadced8d2eeabd2bafe65df3d954df32d2f
Carhor <1029752365@qq.com>
2020-12-26 23:29:48 +0800

    完成用户列表功能开发

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -3,17 +3,30 @@
     <!-- 头像区域 -->
     <div class="login_box">
       <div class="avatar_box">
-        <img src="../assets/logo.png" alt="">
+        <img src="../assets/logo.png" alt="" />
       </div>
       <!-- 表单区域 -->
-      <el-form ref="loginFormRef" :model="loginForm" :rules="loginFormRules" label-width="0px" class="login_form">
+      <el-form
+        ref="loginFormRef"
+        :model="loginForm"
+        :rules="loginFormRules"
+        label-width="0px"
+        class="login_form"
+      >
         <!-- 用户 -->
         <el-form-item prop="username">
-          <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user"></el-input>
+          <el-input
+            v-model="loginForm.username"
+            prefix-icon="iconfont icon-user"
+          ></el-input>
         </el-form-item>
         <!-- 密码 -->
         <el-form-item prop="password">
-          <el-input v-model="loginForm.password" prefix-icon="iconfont icon-3702mima" type="password"></el-input>
+          <el-input
+            v-model="loginForm.password"
+            prefix-icon="iconfont icon-3702mima"
+            type="password"
+          ></el-input>
         </el-form-item>
         <!-- 按钮区域 -->
         <el-form-item class="btns">
@@ -31,45 +44,55 @@ export default {
     return {
       //这是表单绑定对象
       loginForm: {
-        username: 'admin',
-        password: '123456'
+        username: "admin",
+        password: "123456",
       },
       //这是表单验证规则对象
       loginFormRules: {
         //验证用户名称是否合法
         username: [
-          { required: true, message: '请输入用户名称', trigger: 'blur' },
-          { min: 3, max: 11, message: '名称长度在 3 到 11 个字符', trigger: 'blur' }
+          { required: true, message: "请输入用户名称", trigger: "blur" },
+          {
+            min: 3,
+            max: 11,
+            message: "名称长度在 3 到 11 个字符",
+            trigger: "blur",
+          },
         ],
         //验证密码是否合法
         passward: [
-          { required: true, message: '请输入密码', trigger: 'blur' },
-          { min: 3, max: 10, message: '密码长度在 3 到 10 个字符', trigger: 'blur' }
-        ]
-      }
-    }
+          { required: true, message: "请输入密码", trigger: "blur" },
+          {
+            min: 3,
+            max: 10,
+            message: "密码长度在 3 到 10 个字符",
+            trigger: "blur",
+          },
+        ],
+      },
+    };
   },
   methods: {
     //点击重置按钮，重置表单
     resetLoginForm() {
       // console.log(this);
-      this.$refs.loginFormRef.resetFields()
+      this.$refs.loginFormRef.resetFields();
     },
+
+    //监听登录按钮
     login() {
-      this.$refs.loginFormRef.validate(async valid => {
+      this.$refs.loginFormRef.validate(async (valid) => {
         // console.log(valid);
-        if (!valid) return
-        const { data: res } = await this.$http.post('login', this.loginForm)
-        // console.log(res);
-        if (res.meta.status != 200) return this.$message.error('登录失败');
-        this.$message.success('登录成功！');
-        // console.log(res);
-        window.sessionStorage.setItem('token', res.data.token);
-        this.$router.push('/home');
+        if (!valid) return;
+        const { data: res } = await this.$http.post("login", this.loginForm);
+        if (res.meta.status !== 200) return this.$message.error("登录失败");
+        this.$message.success("登录成功！");
+        window.sessionStorage.setItem("token", res.data.token);
+        this.$router.push("/home");
       });
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style lang="less" scoped>
@@ -86,9 +109,9 @@ export default {
   position: absolute;
   left: 50%;
   top: 50%;
-  transform: translate(-50%,-50%);
+  transform: translate(-50%, -50%);
 
-  .avatar_box{
+  .avatar_box {
     height: 130px;
     width: 130px;
     border: 1px solid #eee;
@@ -99,7 +122,7 @@ export default {
     left: 50%;
     transform: translate(-50%, -50%);
     background-color: #fff;
-    img{
+    img {
       width: 100%;
       height: 100%;
       border-radius: 50%;
@@ -107,15 +130,15 @@ export default {
     }
   }
 
-.login_form {
-  position: absolute;
-  bottom: 0;
-  width: 100%;
-  padding: 0 20px;
-  box-sizing: border-box;
-}
+  .login_form {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    padding: 0 20px;
+    box-sizing: border-box;
+  }
 
-.btns {
+  .btns {
     display: flex;
     justify-content: flex-end;
   }
